3053ccf73ff1e3e75cb32b32059b92478ba91db7
mamunabdullah <mamunsarkar18@gmail.com>
2020-08-24 17:49:36 +0600

    Initial Commit

---
+++ SSLDemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSLDemoProject
+//
+//  Created by Abdullah Al Mamun on 24/8/20.
+//  Copyright © 2020 Abdullah Al Mamun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
